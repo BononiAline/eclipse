df14d30ba018ef7dd944b0d7a006287494405e62
BononiAline <50929356+BononiAline@users.noreply.github.com>
2020-08-18 20:06:38 -0300

    Adicionei via upload

---
+++ Hello GitHub/src/bononialine/git/HelloGit.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package bononialine.git;
+
+/**
+ * @author aline
+ *Exemplo de integração do Eclipse com o GituHub
+ */
+public class HelloGit {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Git");
+
+	}
+
+}
